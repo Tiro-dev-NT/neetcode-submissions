736da089132bd02abf933ddffb86988692d2c567
Tiro <dev.tiro.06@gmail.com>
2026-07-18 08:56:26 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_new = ""
+        for c in s:
+            if c.isalnum():
+                s_new += c.lower()
+ 
+        return s_new == s_new[::-1]    
